aca7a872a8fbec9572596c6ab5c232b0d9d9036e
Rupinder Virdi <35664372+rockingvirdi96@users.noreply.github.com>
2020-11-20 23:25:21 -0500

    Updates

---
+++ precache-manifest.3b323463eec8bbd6db0df4084de5c0f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d1cc9e5d97822f27828ae9fb966c15",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "65232535676b35e8083c",
+    "url": "/hangman/static/css/main.119b9e78.chunk.css"
+  },
+  {
+    "revision": "04cd57426d7a08b9c87b",
+    "url": "/hangman/static/js/2.12a0ba66.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/hangman/static/js/2.12a0ba66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65232535676b35e8083c",
+    "url": "/hangman/static/js/main.c33693d8.chunk.js"
+  },
+  {
+    "revision": "e8e5f2263c89dea7c028",
+    "url": "/hangman/static/js/runtime-main.395feef2.js"
+  },
+  {
+    "revision": "f050bb955e7a8205718a6d7a6c2fd08c",
+    "url": "/hangman/static/media/darkMode.f050bb95.svg"
+  },
+  {
+    "revision": "f6dd9a75881b2dd3a64c5c1e5649a05c",
+    "url": "/hangman/static/media/hint.f6dd9a75.svg"
+  },
+  {
+    "revision": "248d8ad5435f983519b7f412cff5b849",
+    "url": "/hangman/static/media/score.248d8ad5.png"
+  }
+]);
